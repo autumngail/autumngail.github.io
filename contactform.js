e017dcb3bed3517123ae56814c72842f19f523d0
autumngail <38111085+autumngail@users.noreply.github.com>
2018-11-21 13:43:53 -0800

    major changes
    created google contact form. editted bio. editted home page. recreated services and prices

---
+++ contactform.js
@@ -0,0 +1,20 @@
+var span = $('<span>').css('display','inline-block')
+.css('word-break','break-all').appendTo('body').css('visibility','hidden');
+function initSpan(textarea){
+  span.text(textarea.text())
+      .width(textarea.width())
+      .css('font',textarea.css('font'));
+}
+$('textarea').on({
+    input: function(){
+      var text = $(this).val();
+      span.text(text);
+      $(this).height(text ? span.height() : '1.1em');
+    },
+    focus: function(){
+     initSpan($(this));
+    },
+    keypress: function(e){
+        if(e.which == 13) e.preventDefault();
+    }
+});
